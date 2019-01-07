14648b9a1670ea4ecb7163ca20ebeda9e4d699df
Yahir Alejandro <yahiralejandros@gmail.com>
2019-01-06 20:11:58 -0600

    Added Albert OBS and Telegram snap

--- install_utils.sh
+++ install_utils.sh
@@ -26,3 +26,14 @@ EOL
 sudo add-apt-repository ppa:vantuz/cool-retro-term
 sudo apt update -y
 sudo apt install cool-retro-term -y
+
+# Telegram
+sudo snap install telegram-desktop
+
+# Albert
+wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_18.04/Release.key -O Release.key
+sudo apt-key add - < Release.key
+sudo apt-get update
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list"
+sudo apt-get update
+sudo apt-get install albert
