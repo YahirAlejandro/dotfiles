65b0fe92526776f74d1a61a622400427d1238192
Yahir <yahiralejandros@gmail.com>
2022-12-14 23:36:29 +0100

    Updated packages

--- i3/wmdeps.sh
+++ i3/wmdeps.sh
@@ -4,8 +4,15 @@
 git config --global user.email "yahiralejandros@gmail.com"
 git config --global user.name "Yahir"
 
-# VSCode via Snap
+# SNAP PACKAGES------------------
+# VSCode
 sudo snap install --classic code
+
+# Spotify
+sudo snap install spotify
+
+# Telegram
+sudo snap install telegram-desktop
 
 # update package manager
 sudo apt-get update
@@ -23,4 +30,17 @@ sudo apt-get install rofi -y
 sudo apt-get install feh -y
 
 # exFAT utilities
-sudo apt-get install exfat-fuse exfatprogs
+sudo apt-get install exfat-fuse exfatprogs -y
+
+# Golang
+sudo apt install golang -y
+
+# DuckDuckGo
+sudo apt-get install ddgr -y
+
+# CUSTOMIZATION----------------------------
+
+# Wallpaper
+sudo apt-get install wget -y
+wget https://i.redd.it/zgedma244ya81.png -P ~/Documents/
+feh --bg-scale ~/Documents/zgedma244ya81.png
